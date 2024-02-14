291febfd1cba4ca7eb289e489aa71e584157e124
Eugenio Crujido Jr <eugene.crujido@gmail.com>
2024-02-14 08:41:35 +0000

    last commit

--- index.js
+++ index.js
@@ -45,14 +45,14 @@ app.get("/api/hello", function (req, res) {
 });
 
 
-// Initialize the arrays to store longUrl and corresponding short longUrl
-const longUrl = [];
+// Initialize the arrays to store URLs and corresponding short URLs
+const Urls = [];
 const shortUrl = [];
 
-// Route for creating short longUrl
-app.post("/api/shorturl", async (req, res) => {
+// Route for creating short URLs
+app.post("/api/shorturl", (req, res) => {
     const url = req.body.url;
-    const urlIndex = longUrl.indexOf(url);
+    const urlIndex = Urls.indexOf(url);
 
     // Check if the URL is valid
     if (!url.startsWith("https://") && !url.startsWith("http://")) {
@@ -61,20 +61,8 @@ app.post("/api/shorturl", async (req, res) => {
 
     // If the URL is not already in the array, add it and generate a short URL
     if (urlIndex < 0) {
-        longUrl.push(url);
+        Urls.push(url);
         shortUrl.push(shortUrl.length);
-
-        // Write each URL to the database
-        try {
-          for (let i = 0; i < longUrl.length; i++) {
-              await Url.create({ original: longUrl[i], short: i + 1 });
-          }
-          console.log("Urls written to the database.");
-          } catch (error) {
-            console.error("Error writing urls to the database:", error);
-            return res.status(500).json({ error: "Internal Server Error" });
-          }
-
         return res.json({
             original_url: url,
             short_url: shortUrl.length - 1,
@@ -86,11 +74,10 @@ app.post("/api/shorturl", async (req, res) => {
         original_url: url,
         short_url: shortUrl[urlIndex],
     });
+});
 
-  });
 
-
-// Route for redirecting short longUrl to original longUrl
+// Route for redirecting short URLs to original URLs
 app.get("/api/shorturl/:url", (req, res) => {
     const url = parseInt(req.params.url);
     const indexFound = shortUrl.indexOf(url);
@@ -101,38 +88,8 @@ app.get("/api/shorturl/:url", (req, res) => {
     }
 
     // Redirect to the original URL corresponding to the short URL
-    res.redirect(longUrl[indexFound]);
+    res.redirect(Urls[indexFound]);
 });
-
-
-
-// function verifyUrl(submittedUrl, callback) {
-//   try {
-//     const parsedUrl = new URL(submittedUrl);
-//     const hostname = parsedUrl.hostname;
-    
-//     dns.lookup(hostname, (err, address, family) => {
-//       if (err) {
-//         callback(err);
-//       } else {
-//         callback(null, { hostname, address, family });
-//       }
-//     });
-//   } catch (error) {
-//     callback(error);
-//   }
-// }
-
-
-// const submittedUrl = 'https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice';
-
-// verifyUrl(submittedUrl, (err, result) => {
-//   if (err) {
-//     console.error('Error:', err.message);
-//   } else {
-//     console.log('Verified URL:', result);
-//   }
-// });
 
 
 app.listen(port, function () {
